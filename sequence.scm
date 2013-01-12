e1792c92d3e397ee1e6029aa25aecdc510b1d027
Hengqing Hu <hudayou@hotmail.com>
2013-01-12 21:15:16 +0800

    Add op properties

--- sequence.scm
+++ sequence.scm
@@ -88,10 +88,13 @@
 
 (define fold-right accumulate)
 
+;; (a op b) op c  = a op (b op c)
+
 (fold-right / 1 (list 1 2 3))
 (fold-left / 1 (list 1 2 3))
 (fold-right list nil (list 1 2 3))
 (fold-left list nil (list 1 2 3))
+
 
 (define (reverse sequence)
   (fold-right (lambda (x y) (append y (list x))) nil sequence))
