a3c416d8405fb1a5b9b4d92a6ed598703c66d12c
Hengqing Hu <hudayou@hotmail.com>
2013-01-15 17:51:51 +0800

    Migrate to hash table

--- find-identical-blocks.scm
+++ find-identical-blocks.scm
@@ -20,30 +20,32 @@
 ;; (hash-of-block path-to-file start-line end-line)
 
 (define (make-block start-line end-line path-to-file content-of-block)
-  (list (string-hash content-of-block) path-to-file start-line end-line))
+  (vector (string-hash content-of-block) path-to-file start-line end-line))
 
 (define (block-hash block)
-  (list-ref block 0))
+  (vector-ref block 0))
 
 (define (block-path block)
-  (list-ref block 1))
+  (vector-ref block 1))
 
 (define (block-start block)
-  (list-ref block 2))
+  (vector-ref block 2))
 
 (define (block-end block)
-  (list-ref block 3))
+  (vector-ref block 3))
 
 (define (block-size block)
-  (- (block-end block) (block-start block)))
+  (if (pair? block)
+    (block-size (car block))
+    (+ 1 (- (block-end block) (block-start block)))))
 
 (define (block-overlaps? block1 block2)
-  (if (>= (block-start block1) (block-start block2))
+  (or
     (and (>= (block-start block1) (block-start block2))
          (<= (block-end block1) (block-end block2))
          (string=? (block-path block1) (block-path block2)))
-    (and (>= (block-start block2) (block-start block1))
-         (<= (block-end block2) (block-end block1))
+    (and (<= (block-start block1) (block-start block2))
+         (>= (block-end block1) (block-end block2))
          (string=? (block-path block1) (block-path block2)))))
 
 (define (block-hash=? block1 block2)
@@ -167,6 +169,97 @@
                     (build-block lineno list-of-lines)))))
   (let ((file-content (call-with-input-file file read-file)))
     (iter 1 (string-split file-content #\newline) nil)))
+
+(define delimiter #\newline)
+(define delimiter-string "\n")
+
+(define (read-lines file)
+  (string-split
+    (string-trim-right
+      (call-with-input-file file read-file)
+      delimiter)
+    delimiter))
+
+(define (build-block-hash-table file)
+  (define block-hash-table (make-hash-table))
+  (define list-of-lines (read-lines file))
+  (define (remove-single-blocks)
+    (hash-for-each
+      (lambda (k v)
+        (if (< (length v) min-repeat-factor)
+          (hash-remove! block-hash-table k))) block-hash-table))
+  (define (build-hash-table line-list start-line)
+    (if (< (length line-list) min-block-size)
+      (begin
+        (remove-single-blocks)
+        block-hash-table)
+      (begin
+        (update-hash-table line-list start-line)
+        (build-hash-table (cdr line-list) (+ start-line 1)))))
+  (define (update-hash-table line-list start-line)
+    (for-each
+      (lambda (b)
+        (let ((b-key (block-hash b)))
+          (let ((b-value (hash-ref block-hash-table b-key)))
+            (if b-value
+              (hash-set! block-hash-table b-key (cons b b-value))
+              (hash-set! block-hash-table b-key (list b))))))
+      (build-blocks line-list start-line)))
+  (define (build-blocks line-list start-line)
+    (map
+      (lambda (bsize)
+        (make-block
+          start-line
+          (+ start-line bsize -1)
+          file
+          (string-join (list-head line-list bsize) delimiter-string)))
+      (enumerate-interval min-block-size
+                          (let ((no-of-lines (length line-list)))
+                            (if (< no-of-lines max-block-size)
+                              no-of-lines
+                              max-block-size)))))
+  (build-hash-table list-of-lines 1))
+
+(define (hash-empty? hash-table)
+  (eq? (hash-map->list cons hash) nil))
+
+(define (remove-duplicate-entries hash-table)
+  (define (remove-overlaps k1 v1)
+    (let ((s1 (block-size v1)))
+      (hash-for-each
+        (lambda (k2 v2)
+          (let ((s2 (block-size v2)))
+            (if (block-list-overlaps? v1 v2)
+              (if (< s2 s1)
+                (hash-remove! hash-table k2)))))
+        hash-table)))
+  (for-each
+    (lambda (x)
+      (remove-overlaps (car x) (cdr x)))
+    (hash-map->list cons hash-table))
+  (for-each
+    (lambda (x)
+      (remove-overlaps (car x) (cdr x)))
+    (hash-map->list cons hash-table))
+  hash-table)
+
+(define (hash-show hash-table)
+  (hash-for-each
+    (lambda (k v)
+      (if (> (length v) 1)
+        (begin
+          (display k)
+          (display "\t***\t")
+          (display v)
+          (newline))))
+    hash-table))
+
+(define (block-list-overlaps? lst1 lst2)
+  (if (and (null? lst1) (null? lst2))
+    #t
+    (and (= (length lst1) (length lst2))
+         (block-overlaps? (car lst1) (car lst2))
+         (block-list-overlaps? (cdr lst1) (cdr lst2)))))
 
 (define (find-identical-blocks file)
   (define (less lst1 lst2)
