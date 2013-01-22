81f3237b29d6cdd40d7291ac67e6c7156f28eb9d
Hengqing Hu <hudayou@hotmail.com>
2013-01-22 13:32:19 +0800

    Add set procedures

---
+++ set.scm
@@ -0,0 +1,46 @@
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (define (union s1 s2)
+    (cond ((null? s1) s2)
+          ((null? s2) s1)
+          ((element-of-set? (car s1) s2)
+           (union (cdr s1) s2))
+          (else (union (cdr s1) (cons (car s1) s2)))))
+  (union (reverse set1) set2))
+
+;; allow duplicate elements in list representation
+;; eg: {1,2,3} could be represented as (2 3 2 1 3 2 2).
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+    (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (append set1 set2))
