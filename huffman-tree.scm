cb9b8cc23e77c78f99576c829a995b015c172fd6
Hengqing Hu <hudayou@hotmail.com>
2013-01-28 19:09:56 +0800

    Work out more exercises

--- huffman-tree.scm
+++ huffman-tree.scm
@@ -87,10 +87,50 @@
 
 (define (successive-merge leaf-set)
   (if (= (length leaf-set) 1)
-    leaf-set
-    (successive-merge 
+    (car leaf-set)
+    (successive-merge
       (adjoin-set
         (make-code-tree
           (car leaf-set)
           (cadr leaf-set))
         (cddr leaf-set)))))
+
+(define lyrics-tree (generate-huffman-tree
+                      '((A 2) (NA 16) (BOOM 1) (SHA 3)
+                              (GET 2) (YIP 9) (JOB 2) (WAH 1))))
+
+(pretty-print lyrics-tree)
+
+(define sample-lyrics '(GET A JOB
+                            SHA NA NA NA NA NA NA NA NA
+                            GET A JOB
+                            SHA NA NA NA NA NA NA NA NA
+                            WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP
+                            SHA BOOM))
+
+(define sample-encoded-lyrics (encode sample-lyrics lyrics-tree))
+
+(pretty-print sample-encoded-lyrics)
+(display "length of sample-encoded-lyrics is ")
+(pretty-print (length sample-encoded-lyrics))
+(display "length of sample-lyrics is ")
+(pretty-print (length sample-lyrics))
+;; 108 bits will be needed
+(pretty-print (* 100 (- 1 (/ 84.0 108))))
+
+;; Suppose we have a Huffman tree for an alphabet of n symbols, and that the
+;; relative frequencies of the symbols are 1, 2, 4, ..., 2^n-1.
+;; 1 bit is needed to encode the most frequent symbol, n-1 bit is required to
+;; encode the least frequent symbol.
+
+;; Consider the encoding procedure that you designed in exercise 2.68. What is
+;; the order of growth in the number of steps needed to encode a symbol? Be
+;; sure to include the number of steps needed to search the symbol list at each
+;; node encountered. To answer this question in general is difficult. Consider
+;; the special case where the relative frequencies of the n symbols are as
+;; described in exercise 2.71, and give the order of growth (as a function of
+;; n) of the number of steps needed to encode the most frequent and least
+;; frequent symbols in the alphabet.
+;;
+;; O(1) steps is needed for most frequent symbol
+;; O(n^2) steps is needed for least frequent symbol
