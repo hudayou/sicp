757e30f987332e8dadb291ad071758d32345d3f4
Hengqing Hu <hudayou@hotmail.com>
2013-02-19 15:45:52 +0800

    Add mutator procedures

---
+++ mutators.scm
@@ -0,0 +1,63 @@
+(define (append x y)
+  (if (null? x)
+    y
+    (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+    x
+    (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
+(cdr x)
+(define w (append! x y))
+w
+(cdr x)
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define (circular-list? x)
+  (define (circle? x y)
+    (cond ((null? (cdr x)) #f)
+          ((eq? (cdr x) y) #t)
+          (else
+            (circle? (cdr x) y))))
+  (circle? x x))
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+      y
+      (let ((temp (cdr x)))
+        (set-cdr! x y)
+        (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+(define w (mystery v))
+(define u (mystery w))
+;; notice the sharing of data between u, v and w
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+z1
+(set-to-wow! z1)
+z2
+(set-to-wow! z2)
+(eq? (car z1) (cdr z1))
+(eq? (car z2) (cdr z2))
