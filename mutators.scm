71fbe9ff22ae508dcaf180686eaba1fbccf4b595
Hengqing Hu <hudayou@hotmail.com>
2013-02-20 15:27:29 +0800

    Solution for 3.20

--- mutators.scm
+++ mutators.scm
@@ -117,3 +117,29 @@ z2
 
 (define o '(o))
 (set-cdr! o o)
+
+;; (define (cons x y)
+;;   (define (set-x! v) (set! x v))
+;;   (define (set-y! v) (set! y v))
+;;   (define (dispatch m)
+;;     (cond ((eq? m 'car) x)
+;;           ((eq? m 'cdr) y)
+;;           ((eq? m 'set-car!) set-x!)
+;;           ((eq? m 'set-cdr!) set-y!)
+;;           (else (error "Undefined operation -- CONS" m))))
+;;   dispatch)
+;; (define (car z) (z 'car))
+;; (define (cdr z) (z 'cdr))
+;; (define (set-car! z new-value)
+;;   ((z 'set-car!) new-value)
+;;   z)
+;; (define (set-cdr! z new-value)
+;;   ((z 'set-cdr!) new-value)
+;;   z)
+;; (define x (cons 1 2))
+;; (define z (cons x x))
+;; (set-car! (cdr z) 17)
+;; (car x)
+;;
+;; cdr z returns the procedure object x, calling set-car! on x,
+;; will set the x binding in x's enclosing frame to be 17.
