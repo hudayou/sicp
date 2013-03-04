021a474f84abfd9bd724f1ff8523d2fd8d2e54e4
Hengqing Hu <hudayou@hotmail.com>
2013-03-04 20:49:59 +0800

    Incorrect enumerate-hamming-integers implementation

--- inf-streams.scm
+++ inf-streams.scm
@@ -61,6 +61,8 @@
   (xes 1))
 (define (add-stream s1 s2)
   (stream-map + s1 s2))
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
 (define (scale-stream stream factor)
   (stream-map (lambda (x) (* x factor)) stream))
 (define (xble x)
@@ -83,3 +85,92 @@
                              (+ (stream-car (car state))
                                 (cdr state)))))
                (cons (stream-cdr stream) (stream-car stream))))
+
+(define enumerate-hamming-integers
+  ;; state is list
+  ;; (hamming-integer
+  ;;   (scale-stream integers 2)
+  ;;   (scale-stream integers 3)
+  ;;   (scale-stream integers 5))
+  (make-stream (lambda (state)
+                 (cons (car state)
+                       (let ((twos (cadr state))
+                             (threes (caddr state))
+                             (fives (cadddr state)))
+                         (let ((first-two (stream-car twos))
+                               (first-three (stream-car threes))
+                               (first-five (stream-car fives)))
+                           (cond ((and (< first-two first-three)
+                                       (< first-two first-five))
+                                  (list first-two
+                                        (stream-cdr twos)
+                                        threes
+                                        fives))
+                                 ((and (< first-three first-two)
+                                       (< first-three first-five))
+                                  (list first-three
+                                        twos
+                                        (stream-cdr threes)
+                                        fives))
+                                 ((and (< first-five first-two)
+                                       (< first-five first-three))
+                                  (list first-five
+                                        twos
+                                        threes
+                                        (stream-cdr fives)))
+                                 ((= first-two first-three first-five)
+                                  (list first-two
+                                        (stream-cdr twos)
+                                        (stream-cdr threes)
+                                        (stream-cdr fives)))
+                                 ((= first-two first-three)
+                                  (list first-two
+                                        (stream-cdr twos)
+                                        (stream-cdr threes)
+                                        fives))
+                                 ((= first-three first-five)
+                                  (list first-three
+                                        twos
+                                        (stream-cdr threes)
+                                        (stream-cdr fives)))
+                                 ((= first-two first-five)
+                                  (list first-five
+                                        (stream-cdr twos)
+                                        threes
+                                        (stream-cdr fives))))))))
+               (list 1
+                     (scale-stream integers 2)
+                     (scale-stream integers 3)
+                     (scale-stream integers 5))))
+
+(define (one-shot-stream bullet)
+  (make-stream (lambda (state)
+                 (list state))
+               bullet))
+
+(define enumerate-hamming-integers
+  (make-stream (lambda (state)
+                 (cons (stream-car state)
+                         (merge-stream (scale-stream state 2)
+                                       (scale-stream state 3))
+                         ))
+               (one-shot-stream 1)))
+
+(define (merge-stream stream1 stream2)
+  (cond ((stream-null? stream1) stream2)
+        ((stream-null? stream2) stream1)
+        (else
+          (make-stream
+            (lambda (state)
+              (let ((s1 (car state))
+                    (s2 (cdr state)))
+                (let ((s1car (stream-car s1))
+                      (s2car (stream-car s2)))
+                  (cond ((< s1car s2car)
+                         (cons s1car (cons (stream-cdr s1) s2)))
+                        ((> s1car s2car)
+                         (cons s2car (cons s1 (stream-cdr s2))))
+                        (else
+                          (cons s1car (cons (stream-cdr s1)
+                                            (stream-cdr s2))))))))
+            (cons stream1 stream2)))))
