4fdab77e91551c3705692957a0fe95004ee95cad
Hengqing Hu <hudayou@hotmail.com>
2013-03-11 13:26:25 +0800

    Add solution for 3.74

--- stream.scm
+++ stream.scm
@@ -403,3 +403,13 @@
                           dt))))
 
 (define RC1 (RC 5 1 0.5))
+
+(define (make-zero-crossings input-stream last-value)
+  (cons-stream
+    (sign-change-detector (stream-car input-stream) last-value)
+    (make-zero-crossings (stream-cdr input-stream)
+                         (stream-car input-stream))))
+(define zero-crossings (make-zero-crossings sense-data 0))
+
+(define zero-crossings
+  (stream-map sign-change-detector sense-data (stream-cdr sense-data)))
