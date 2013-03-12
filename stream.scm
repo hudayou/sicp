10aeafeae0c3b2cce6b27015c6d80c8d0b0b8ff4
Hengqing Hu <hudayou@hotmail.com>
2013-03-12 13:37:41 +0800

    Add solution for 3.77-3.80

--- stream.scm
+++ stream.scm
@@ -438,3 +438,41 @@
                 (/ (+ x y) 2))
               stream
               (stream-cdr stream)))
+
+;; solution for 3.77
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                   the-empty-stream
+                   (integral (delay (stream-cdr integrand))
+                             (+ (* dt (stream-car integrand))
+                                initial-value)
+                             dt)))))
+
+;; solution for 3.78
+(define (solve-2nd a b dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 (square dt)))
+  (define ddy (add-stream (scale-stream dy a)
+                          (scale-stream y b)))
+  y)
+
+;; solution for 3.79
+(define (solve-2nd f dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 (square dt)))
+  (define ddy (stream-map f dy y))
+  y)
+
+;; solution for 3.80
+(define (RLC r l c dt)
+  (lambda (vc0 il0)
+    (define vc (integral (delay dvc) vc0 dt))
+    (define il (integral (delay dil) il0 dt))
+    (define dvc (scale-stream il (- (/ 1 c))))
+    (define dil (add-stream (scale-stream vc (/ 1 l))
+                            (scale-stream il (- (/ r l)))))
+    (cons vc il)))
+
+(define RLC1 ((RLC 1 1 0.2 0.1) 10 1))
